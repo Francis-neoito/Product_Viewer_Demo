abb5b4a2530e4ea345b6cb99796d31566891824e
francis-neoito <francis.xavier@neoito.com>
2023-10-09 21:42:39 +0530

    webgi implementation and obj addition

--- src/components/controllers/collectionApp.js
+++ src/components/controllers/collectionApp.js
@@ -1,6 +1,7 @@
 
 import products from './productData';
 import * as THREE from 'three';
+import * as WEBGI from 'webgi';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -76,151 +77,69 @@ const initCollectionApp = function(){
         props:['productData'],
         data(){
             return{
+                isLoaded : false,
+                loadPercentage:0,
             }
         },
         created(){
-            document.addEventListener('stoprendering',(e)=>{
-                this.stopRendering();
-            });
-            // document.addEventListener('resetviewer',(e)=>{
-            //     this.stopRendering();
-            // });
         },
         mounted(){
             this.dom = document.getElementById("viewerScene");
-            this.initScene();
+            this.initWebGI();
         },
-        methods:{
-            initScene(){
-                const canvaWidth = this.dom.getBoundingClientRect().width;
-                const canvaHeight = this.dom.getBoundingClientRect().height;
-                this.scene = new THREE.Scene();
-                RectAreaLightUniformsLib.init();
-                // this.camera = new THREE.OrthographicCamera(canvaWidth / - 35, canvaWidth / 35, canvaHeight / 35, canvaHeight / - 35, 0.1, 2000);
-                this.camera = new THREE.PerspectiveCamera(50, canvaWidth /  canvaHeight, 0.1, 2000);
-                this.renderer = new THREE.WebGLRenderer({antialias:true});
-                this.renderer.physicallyCorrectLights = true;
-                this.renderer.setSize( canvaWidth, canvaHeight );
-                this.renderer.domElement.style.height = `${canvaHeight} + 'px'`;
-                this.renderer.domElement.style.width = `${canvaWidth} + 'px'`; 
-                this.renderer.domElement.width = canvaWidth;
-                this.renderer.domElement.height = canvaHeight;
-                this.dom.appendChild( this.renderer.domElement );
-                this.renderer.setClearColor( 0x000000, 0 );
-
-                this.composer = new EffectComposer( this.renderer );
-                this.composer.setPixelRatio(window.devicePixelRatio);
-                // this.composer.setSize(canvaWidth, canvaHeight);
-                const renderPass = new RenderPass( this.scene, this.camera );
-                this.composer.addPass( renderPass );
-
-                this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-                this.ambLight = new THREE.AmbientLight( 0xffffff ); // soft white light
-                this.scene.add( this.ambLight );
-                // const axesHelper = new THREE.AxesHelper( 5 );
-                // this.scene.add( axesHelper );
-                // this.camera.position.set(0,0,100);
-                this.camera.position.set(0, 0, 50);
-                this.camera.zoom = 1.8;
-                this.camera.updateProjectionMatrix();
-                if(this.productData.objModelName!=null){
-                    this.loadProductGltfModel();
-                    // this.loadProductObjModel();
-                }
-                const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
-                pmremGenerator.compileCubemapShader();
-                THREE.DefaultLoadingManager.onLoad = function ( ) {
-					pmremGenerator.dispose();
-				};
-                const hdrCubeMap = new RGBELoader()
-					.setPath( './images/' )
-					.load( 'aircraft_workshop_01_1k.hdr', this.envLoadComplete);
-
-                this.controls.autoRotate = true;
-                this.controls.autoRotateSpeed = 1.5;
-                this.controls.minDistance = 30;
-                this.controls.maxDistance  = 100;
-                this.controls.enablePan = false;
-                this.startRendering();
-            },
-            envLoadComplete(texture){
-                texture.mapping = THREE.EquirectangularReflectionMapping;
-                this.scene.environment = texture;
-            },
-            loadComplete(gltf){
-                this.meshes= gltf.scene.children[0].children[0].children[0].children;
-                this.scene.add( gltf.scene );
-                gltf.scene.position.y = 0;
-                gltf.scene.scale.set(7,7,7);
-                gltf.scene.rotation.set(-20*Math.PI/180,-10*Math.PI/180,0);
-                // console.log(this.meshes);
-                // this.meshes[6].material.color = new THREE.Color(0.1,0.2,0.8);
-                // this.meshes[7].material.color = new THREE.Color(0.8,0.7,0.1);
-            },
-            objLoadComplete(object){
-                this.scene.add( object );
-                object.position.y = -50;
-                object.scale.set(0.1,0.1,0.1);
-            },
-            loadProductObjModel(){
-                const loader = new OBJLoader();
-                loader.load(
-                    // resource URL
-                    './objects/' + this.productData.objModelName,
-                    // called when resource is loaded
-                    this.objLoadComplete,
-                    // called when loading is in progresses
-                    function ( xhr ) {
-                    
-                        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-                
-                    },
-                    // called when loading has errors
-                    function ( error ) {
-                
-                        console.log( 'An error happened' );
-                
-                    }
-                );
-            },
-            loadProductGltfModel(){
-                const loader = new GLTFLoader();
-                const dracoLoader = new DRACOLoader();
-                dracoLoader.setDecoderConfig({ type: 'js' });
-                dracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/v1/decoders/' );
-                loader.setDRACOLoader( dracoLoader );
-                loader.load(
-                    // resource URL
-                    './objects/' + this.productData.objModelName,
-                    // called when the resource is loaded
-                    this.loadComplete,
-                    // called while loading is progressing
-                    function ( xhr ) {
-
-                        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-                    },
-                    // called when loading has errors
-                    function ( error ) {
-
-                        console.log( 'An error happened' );
-
-                    }
-                );
-            },
-            animate(){
-                this.renderer.render( this.scene, this.camera );
-                this.controls.update();
-            },
-            startRendering(){
-                this.renderer.setAnimationLoop(this.animate);
-            },
-            stopRendering(){
-                this.renderer.setAnimationLoop(null);
+        unmounted(){
+            if(this.viewer!=null){
+                this.viewer.renderer.refreshPipeline();
+                this.viewer.scene.disposeSceneModels()
+                this.viewer.dispose();
             }
         },
+        watch:{
+            loadPercentage: function(val){
+                const loader = document.getElementById('sceneLoaderBar');
+                if(loader !=null){
+                    loader.style.width = this.loadPercentage + '%';
+                }
+                if(val===100){
+                    this.isLoaded = true;
+                }
+            },
+        }, 
+        methods:{
+            async initWebGI(){
+                this.loadPercentage = 15;
+                this.viewer = new WEBGI.ViewerApp({
+                    canvas: document.getElementById('viewerScene'),
+                    isAntialiased: true,
+                    useRgbm: true
+                  }) ;
+                const manager = new WEBGI.AssetManagerPlugin();
+                await this.viewer.addPlugin(manager);
+                this.viewer.renderer.displayCanvasScaling = Math.min(window.devicePixelRatio, 2);
+                await WEBGI.addBasePlugins(this.viewer);
+                const camViews = this.viewer.getPlugin(WEBGI.CameraViewPlugin);
+                this.viewer.renderer.refreshPipeline();
+                this.viewer.getPlugin(WEBGI.TonemapPlugin).config.clipBackground = true;
+                const options = {autoScale: false}
+                const assets = await manager.addFromPath("./objects/"+ this.productData.objModelName, options);
+                this.loadPercentage = 80;
+                // this.viewer.scene.environment = await manager.importer.importSingle({path:'./images/gem_2.hdr'});
+                this.loadPercentage = 90;
+                this.controls = this.viewer.scene.activeCamera.controls;
+                this.controls.autoRotate = this.turnTable;
+                this.controls.autoRotateSpeed = 0.8;
+                if(!this.bigCanvas){
+                    this.controls.zoomOut(1);
+                    this.controls.zoomOut(1);
+                }
+                this.loadPercentage = 100;
+            },
+        },
         template:`
-            <div id="viewerScene"></div>
+            <div id="sceneLoader" v-if="!isLoaded">
+                <div id="sceneLoaderBar"></div>
+            </div>
+            <canvas id="viewerScene"></canvas>
         `
     });
 
@@ -266,7 +185,7 @@ const initCollectionApp = function(){
                 return true;
             },
             closeViewer(){
-                document.dispatchEvent(new Event('stoprendering'));
+                // document.dispatchEvent(new Event('stoprendering'));
                 this.activeProduct = null;
                 this.activeProductId = null;
                 this.showProductMode = false;
@@ -394,5 +313,137 @@ const initCollectionApp = function(){
     });
     return app;
 }
+
+/***************************
+ * 
+ * backup old code
+ * initScene(){
+                const canvaWidth = this.dom.getBoundingClientRect().width;
+                const canvaHeight = this.dom.getBoundingClientRect().height;
+                this.scene = new THREE.Scene();
+                RectAreaLightUniformsLib.init();
+                // this.camera = new THREE.OrthographicCamera(canvaWidth / - 35, canvaWidth / 35, canvaHeight / 35, canvaHeight / - 35, 0.1, 2000);
+                this.camera = new THREE.PerspectiveCamera(50, canvaWidth /  canvaHeight, 0.1, 2000);
+                this.renderer = new THREE.WebGLRenderer({antialias:true});
+                this.renderer.physicallyCorrectLights = true;
+                this.renderer.setSize( canvaWidth, canvaHeight );
+                this.renderer.domElement.style.height = `${canvaHeight} + 'px'`;
+                this.renderer.domElement.style.width = `${canvaWidth} + 'px'`; 
+                this.renderer.domElement.width = canvaWidth;
+                this.renderer.domElement.height = canvaHeight;
+                this.dom.appendChild( this.renderer.domElement );
+                this.renderer.setClearColor( 0x000000, 0 );
+
+                this.composer = new EffectComposer( this.renderer );
+                this.composer.setPixelRatio(window.devicePixelRatio);
+                // this.composer.setSize(canvaWidth, canvaHeight);
+                const renderPass = new RenderPass( this.scene, this.camera );
+                this.composer.addPass( renderPass );
+
+                this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+                this.ambLight = new THREE.AmbientLight( 0xffffff ); // soft white light
+                this.scene.add( this.ambLight );
+                // const axesHelper = new THREE.AxesHelper( 5 );
+                // this.scene.add( axesHelper );
+                // this.camera.position.set(0,0,100);
+                this.camera.position.set(0, 0, 50);
+                this.camera.zoom = 1.8;
+                this.camera.updateProjectionMatrix();
+                if(this.productData.objModelName!=null){
+                    this.loadProductGltfModel();
+                    // this.loadProductObjModel();
+                }
+                const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
+                pmremGenerator.compileCubemapShader();
+                THREE.DefaultLoadingManager.onLoad = function ( ) {
+					pmremGenerator.dispose();
+				};
+                const hdrCubeMap = new RGBELoader()
+					.setPath( './images/' )
+					.load( 'aircraft_workshop_01_1k.hdr', this.envLoadComplete);
+
+                this.controls.autoRotate = true;
+                this.controls.autoRotateSpeed = 1.5;
+                this.controls.minDistance = 30;
+                this.controls.maxDistance  = 100;
+                this.controls.enablePan = false;
+                this.startRendering();
+            },
+            envLoadComplete(texture){
+                texture.mapping = THREE.EquirectangularReflectionMapping;
+                this.scene.environment = texture;
+            },
+            loadComplete(gltf){
+                this.meshes= gltf.scene.children[0].children[0].children[0].children;
+                this.scene.add( gltf.scene );
+                gltf.scene.position.y = 0;
+                gltf.scene.scale.set(7,7,7);
+                gltf.scene.rotation.set(-20*Math.PI/180,-10*Math.PI/180,0);
+                // console.log(this.meshes);
+                // this.meshes[6].material.color = new THREE.Color(0.1,0.2,0.8);
+                // this.meshes[7].material.color = new THREE.Color(0.8,0.7,0.1);
+            },
+            objLoadComplete(object){
+                this.scene.add( object );
+                object.position.y = -50;
+                object.scale.set(0.1,0.1,0.1);
+            },
+            loadProductObjModel(){
+                const loader = new OBJLoader();
+                loader.load(
+                    // resource URL
+                    './objects/' + this.productData.objModelName,
+                    // called when resource is loaded
+                    this.objLoadComplete,
+                    // called when loading is in progresses
+                    function ( xhr ) {
+                    
+                        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+                
+                    },
+                    // called when loading has errors
+                    function ( error ) {
+                
+                        console.log( 'An error happened' );
+                
+                    }
+                );
+            },
+            loadProductGltfModel(){
+                const loader = new GLTFLoader();
+                const dracoLoader = new DRACOLoader();
+                dracoLoader.setDecoderConfig({ type: 'js' });
+                dracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/v1/decoders/' );
+                loader.setDRACOLoader( dracoLoader );
+                loader.load(
+                    // resource URL
+                    './objects/' + this.productData.objModelName,
+                    // called when the resource is loaded
+                    this.loadComplete,
+                    // called while loading is progressing
+                    function ( xhr ) {
+
+                        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+                    },
+                    // called when loading has errors
+                    function ( error ) {
+
+                        console.log( 'An error happened' );
+
+                    }
+                );
+            },
+            animate(){
+                this.renderer.render( this.scene, this.camera );
+                this.controls.update();
+            },
+            startRendering(){
+                this.renderer.setAnimationLoop(this.animate);
+            },
+            stopRendering(){
+                this.renderer.setAnimationLoop(null);
+            }
+ */
 
 export {initCollectionApp, rootCollectionApp};
